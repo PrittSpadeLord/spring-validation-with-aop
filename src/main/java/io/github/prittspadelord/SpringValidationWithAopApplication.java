8f996461426cba824f54a1895113ad3ac8532992
PrittSpadeLord <pritt1999@gmail.com>
2025-10-12 00:55:14 +0530

    the ship has sailed

---
+++ src/main/java/io/github/prittspadelord/SpringValidationWithAopApplication.java
@@ -0,0 +1,8 @@
+package io.github.prittspadelord;
+
+public class SpringValidationWithAopApplication {
+    static void main() {
+        System.out.println("The goal is simple, run validation checks on input to a method using AOP");
+        System.out.println("Use said AOP to prevent method execution if validation fails");
+    }
+}
